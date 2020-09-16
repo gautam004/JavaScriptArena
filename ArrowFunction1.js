cc8e3b3210beb6fe075f43e79e209122d300f506
gautam004 <gautam.cs16@gmail.com>
2020-09-16 12:41:52 +0530

    simple arrow function

--- ArrowFunction1.js
+++ ArrowFunction1.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+function sum(a , b) {
+    return a+b;
+}
+
+console.log(sum(2,3));
+
+//corresponding arrow function
+
+const sum1 = (a, b) => a+b;
+
+console.log(sum1(2, 3));
