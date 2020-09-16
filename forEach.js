07b9c26ec9ea95e07b97a7fe155bf595e2f783cf
gautam004 <gautam.cs16@gmail.com>
2020-09-16 22:04:15 +0530

    forEach in JS is bit funny though :p

---
+++ forEach.js
@@ -0,0 +1,24 @@
+//https://www.w3schools.com/jsref/jsref_foreach.asp
+
+const allNumbers = [ 1, 2, 3, 4, 5];
+
+//using one paramter of function inside forEach
+let sum = 0;
+allNumbers.forEach( num => {
+    sum = sum + num;
+} )
+console.log(sum);
+
+//using first 2 args
+allNumbers.forEach( (num, index) => {
+    allNumbers[index] = 2*num;
+} )
+allNumbers.forEach( (num, index) => {
+    console.log(allNumbers[index]);
+ } )
+
+ //using 3 args for fn
+allNumbers.forEach( (num, index, arr1) => {
+    arr1[index] = allNumbers[index] + 1;
+    console.log(arr1[index]);
+} )
