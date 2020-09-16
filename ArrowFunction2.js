7f187b54eb17e36ecd786303b6665202e29c1770
gautam004 <gautam.cs16@gmail.com>
2020-09-16 12:47:36 +0530

    just another

---
+++ ArrowFunction2.js
@@ -0,0 +1,11 @@
+function isPositive(num) {
+    return num>0;
+}
+
+console.log(isPositive(2));
+
+//corresponding arrow function
+
+const isPositive1 = num1 => num1>0;
+
+console.log(isPositive1(3));
