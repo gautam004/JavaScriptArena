ba6aa2df137ab54d5117cbb9be0da0a2e302960a
gautam004 <gautam.cs16@gmail.com>
2020-09-16 12:53:58 +0530

    Arrow fn with no params

---
+++ ArrowFunction3.js
@@ -0,0 +1,11 @@
+function randomNumber() {
+    return Math.random;
+}
+
+console.log(randomNumber());
+
+//corresponding arrow function
+
+const randomNumber1 = () => Math.random;
+
+console.log(randomNumber());
